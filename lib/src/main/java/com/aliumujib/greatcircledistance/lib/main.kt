03ee47d655b0a2587d9225191c187358fb632eb3
aliumujib <aliuabdulmujib@yahoo.com>
2020-05-31 17:38:30 +0100

    Removed app module and made simple command line runner

--- lib/src/main/java/com/aliumujib/greatcircledistance/lib/main.kt
+++ lib/src/main/java/com/aliumujib/greatcircledistance/lib/main.kt
@@ -1,14 +1,17 @@
 package com.aliumujib.greatcircledistance.lib
 
 import com.aliumujib.greatcircledistance.lib.main.GreatCircleDistance
+import java.nio.file.Paths
 
 fun main(args: Array<String>) {
+    val workingDir =   Paths.get("").toAbsolutePath().toString()
     val greatCircleDistance = GreatCircleDistance.getInstance()
-    greatCircleDistance.init("/Users/aliumujib/AndroidStudioProjects/GreatCircleDistance/lib/src/main/resources")
+    greatCircleDistance.init("${workingDir}/lib/src/main/resources")
     val results = greatCircleDistance.fetchEligibleCustomers(
-        "/Users/aliumujib/AndroidStudioProjects/GreatCircleDistance/lib/src/main/resources/customers.txt",
+        "${workingDir}/lib/src/main/resources/customers.txt",
         53.339428,
         -6.257664,
         100.0
     )
+
 }
