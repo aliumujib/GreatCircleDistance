6592b7a2317dd21d86baa4c25ab5a367d027acf3
aliumujib <aliuabdulmujib@yahoo.com>
2020-06-02 13:25:47 +0100

    Slight improvments to instance creation method

--- lib/src/main/java/com/aliumujib/greatcircledistance/lib/models/Customer.kt
+++ lib/src/main/java/com/aliumujib/greatcircledistance/lib/models/Customer.kt
@@ -6,5 +6,5 @@ data class Customer(
     val longitude: String,
     val name: String,
     val user_id: Int,
-    var distance_from_location: Double = Double.MAX_VALUE
+    val distance_from_location: Double = Double.MAX_VALUE
 )
